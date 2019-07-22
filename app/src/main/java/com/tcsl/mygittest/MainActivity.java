e59ab00d82cc5e25a14970b68b20910d1a75ca8c
chengshenghao <1151799682@qq.com>
2019-07-22 15:22:53 +0800

    new：添加功能1

---
+++ app/src/main/java/com/tcsl/mygittest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tcsl.mygittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("我是功能1");
+    }
+}
