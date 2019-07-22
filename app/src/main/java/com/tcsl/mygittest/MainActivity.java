f9a1c5d5f64fa512c90daefaffe1698ce7eef04d
chengshenghao <1151799682@qq.com>
2019-07-22 15:53:56 +0800

    new：新增功能4

--- app/src/main/java/com/tcsl/mygittest/MainActivity.java
+++ app/src/main/java/com/tcsl/mygittest/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println("我是功能1");
         System.out.println("我是功能3");
+        System.out.println("我是功能4");
     }
 }
